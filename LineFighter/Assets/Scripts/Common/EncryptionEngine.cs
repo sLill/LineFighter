b2384ee586b65068c8b93c62454ed4617373f164
Samuel Turner-Lill <ssl@ksu.edu>
2018-12-31 13:50:25 -0600

    -Encryption Engine

--- LineFighter/Assets/Scripts/Common/EncryptionEngine.cs
+++ LineFighter/Assets/Scripts/Common/EncryptionEngine.cs
@@ -1,13 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
 public static class EncryptionEngine
 {
+    private const string _hash = "SHA1";
+    private const string _initialVector = "OhLna7^m*bDD$12s";
+    private const int _iterations = 2;
+    private const int _keySize = 256;
+    private const string _password = "NotJustABoulder";
+    private const string _salt = "ItsARock";
+
+
+    public static string Decrypt(string encryptedText)
+    {
+        int ByteCount = 0;
+        byte[] PlainTextBytes = null;
+        RijndaelManaged SymmetricKey = new RijndaelManaged();
+
+        if (!string.IsNullOrEmpty(encryptedText))
+        {
+            byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(_initialVector);
+            byte[] SaltValueBytes = Encoding.ASCII.GetBytes(_salt);
+            byte[] CipherTextBytes = Convert.FromBase64String(encryptedText);
+            PlainTextBytes = new byte[CipherTextBytes.Length];
+            PasswordDeriveBytes DerivedPassword = new PasswordDeriveBytes(_password, SaltValueBytes, _hash, _iterations);
+            byte[] KeyBytes = DerivedPassword.GetBytes(_keySize / 8);
+            SymmetricKey.Mode = CipherMode.CBC;
+            using (ICryptoTransform Decryptor = SymmetricKey.CreateDecryptor(KeyBytes, InitialVectorBytes))
+            {
+                using (MemoryStream MemStream = new MemoryStream(CipherTextBytes))
+                {
+                    using (CryptoStream CryptoStream = new CryptoStream(MemStream, Decryptor, CryptoStreamMode.Read))
+                    {
+                        ByteCount = CryptoStream.Read(PlainTextBytes, 0, PlainTextBytes.Length);
+                        MemStream.Close();
+                        CryptoStream.Close();
+                    }
+                }
+            }
+        }
+
+        SymmetricKey.Clear();
+        return Encoding.UTF8.GetString(PlainTextBytes, 0, ByteCount);
+    }
+
     public static string Encrypt(string rawText)
     {
-        return string.Empty;
+        RijndaelManaged SymmetricKey = new RijndaelManaged();
+        byte[] CipherTextBytes = null;
+
+        if (!string.IsNullOrEmpty(rawText))
+        {
+            byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(_initialVector);
+            byte[] SaltValueBytes = Encoding.ASCII.GetBytes(_salt);
+            byte[] PlainTextBytes = Encoding.UTF8.GetBytes(rawText);
+            PasswordDeriveBytes DerivedPassword = new PasswordDeriveBytes(_password, SaltValueBytes, _hash, _iterations);
+            byte[] KeyBytes = DerivedPassword.GetBytes(_keySize / 8);
+            SymmetricKey.Mode = CipherMode.CBC;
+
+            using (ICryptoTransform Encryptor = SymmetricKey.CreateEncryptor(KeyBytes, InitialVectorBytes))
+            {
+                using (MemoryStream MemStream = new MemoryStream())
+                {
+                    using (CryptoStream CryptoStream = new CryptoStream(MemStream, Encryptor, CryptoStreamMode.Write))
+                    {
+                        CryptoStream.Write(PlainTextBytes, 0, PlainTextBytes.Length);
+                        CryptoStream.FlushFinalBlock();
+                        CipherTextBytes = MemStream.ToArray();
+                        MemStream.Close();
+                        CryptoStream.Close();
+                    }
+                }
+            }
+        }
+
+        SymmetricKey.Clear();
+        return Convert.ToBase64String(CipherTextBytes);
     }
 }
